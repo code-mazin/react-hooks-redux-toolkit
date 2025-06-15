f2f6fc67dcfea2b4c09442cb01b40c4d8064bac9
Mazin <code.mazin.m@gmail.com>
2025-06-15 17:08:03 +1000

    test suites r busted

--- src/features/cats/catsSlice.js
+++ src/features/cats/catsSlice.js
@@ -1,34 +1,43 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 // Action Creators
 
 // async actions
-export function fetchCats() {
-  return function (dispatch) {
-    dispatch({ type: "cats/fetchCats/pending" });
-    fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
-      .then((response) => response.json())
-      .then((data) => {
-        dispatch({
-          type: "cats/fetchCats/fulfilled",
-          payload: data.images,
-        });
-      });
-  };
-}
 
-// sync actions added for demo purposes
-export function catAdded(newCat) {
-  return {
-    type: "cats/catAdded",
-    payload: newCat,
-  };
-}
+export const fetchCats = createAsyncThunk("cats/fetchCats", () => {
+  //return a Promise containing the data we want
+  return fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+    .then((response) => response.json())
+    .then((data) => data.images);
+});
 
-export function catUpdated(updatedCat) {
-  return {
-    type: "cats/catUpdated",
-    payload: updatedCat,
-  };
-}
+// export function fetchCats() {
+//   return function (dispatch) {
+//     dispatch({ type: "cats/fetchCats/pending" });
+//     fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         dispatch({
+//           type: "cats/fetchCats/fulfilled",
+//           payload: data.images,
+//         });
+//       });
+//   };
+// }
+
+// // sync actions added for demo purposes
+// export function catAdded(newCat) {
+//   return {
+//     type: "cats/catAdded",
+//     payload: newCat,
+//   };
+// }
+
+// export function catUpdated(updatedCat) {
+//   return {
+//     type: "cats/catUpdated",
+//     payload: updatedCat,
+//   };
+// }
 
 // Reducer
 const initialState = {
@@ -36,38 +45,70 @@ const initialState = {
   status: "idle", // loading state
 };
 
-function catsReducer(state = initialState, action) {
-  switch (action.type) {
-    // sync actions
-    case "cats/catAdded":
-      return {
-        ...state,
-        entities: [...state.entities, action.payload],
-      };
-    case "cats/catUpdated":
-      return {
-        ...state,
-        entities: state.entities.map((cat) =>
-          cat.id === action.payload.id ? action.payload : cat
-        ),
-      };
 
-    // async actions
-    case "cats/fetchCats/pending":
-      return {
-        ...state,
-        status: "loading",
-      };
-    case "cats/fetchCats/fulfilled":
-      return {
-        ...state,
-        entities: action.payload,
-        status: "idle",
-      };
+// function catsReducer(state = initialState, action) {
+//   switch (action.type) {
+//     // sync actions
+//     case "cats/catAdded":
+//       return {
+//         ...state,
+//         entities: [...state.entities, action.payload],
+//       };
+//     case "cats/catUpdated":
+//       return {
+//         ...state,
+//         entities: state.entities.map((cat) =>
+//           cat.id === action.payload.id ? action.payload : cat
+//         ),
+//       };
 
-    default:
-      return state;
-  }
-}
+//     // async actions
+//     case "cats/fetchCats/pending":
+//       return {
+//         ...state,
+//         status: "loading",
+//       };
+//     case "cats/fetchCats/fulfilled":
+//       return {
+//         ...state,
+//         entities: action.payload,
+//         status: "idle",
+//       };
 
-export default catsReducer;
+//     default:
+//       return state;
+//   }
+// }
+
+const catsSlice = createSlice({
+  name: "cats",
+  initialState,
+  reducers: {
+    //sync reducers
+    catAdded(state, action) {
+      //using createSlice lets us mutate state!
+      state.entities.push(action.payload);
+    },
+    catUpdated(state, action) {
+      const cat = state.entities.find((cat) => cat.id === action.payload.id);
+      cat.url = action.payload.url
+    },
+  },
+  extraReducers: {
+    // handle async action types
+    [fetchCats.pending](state) {
+      state.status = "loading";
+    },
+    [fetchCats.fulfilled](state, action) {
+      state.entities = action.payload;
+      state.status = "idle";
+    },
+  },
+});
+
+
+// export default catsReducer;
+
+export const { catAdded, catUpdated } = catsSlice.actions;
+
+export default catsSlice.reducer;
